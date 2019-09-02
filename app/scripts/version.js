4da28dabf32515604e07f98a3427241f4e7555bb
McRaZick <mcrazick@mail.com>
2019-09-02 16:55:56 +0100

    Update version.js

--- app/scripts/version.js
+++ app/scripts/version.js
@@ -1,13 +1,13 @@
-var releasedVersion = '2.5.1';
+var releasedVersion = '2.5.2';
 
 function updatePublishedVersion(releasedVersion) {
-  var publishedVersion = '2.5.1';
+  var publishedVersion = '2.5.2';
   while (publishedVersion != releasedVersion) { publishedVersion = releasedVersion }
   return publishedVersion;
 }
 
 function updateCurrentVersion(releasedVersion) {
-  var currentVersion = '2.5.1';
+  var currentVersion = '2.5.2';
   while (currentVersion != releasedVersion) { currentVersion = releasedVersion }
   return currentVersion;
 }
@@ -15,7 +15,6 @@ function updateCurrentVersion(releasedVersion) {
 function updateDocumentation() {
   $("#aboutNewRelease").html("\
     <br/>\
-    <li>New design has been implemented for display of aboutMDT.html and for option \"check for updates\"</li><br/>\
-    <li>Removed major script junk and improved documentation</li><br/>\
+    <li>Updated development tools images and the logo. Changed word \"Developing\" to \"Development\"</li><br/>\
   ");
 }
