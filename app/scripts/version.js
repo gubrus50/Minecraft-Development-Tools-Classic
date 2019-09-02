0b680f9043e9e0b09bb7b44094dc14b640c57ffa
McRaZick <mcrazick@mail.com>
2019-09-02 18:35:24 +0100

    Update Launcher 2.5.2

--- app/scripts/version.js
+++ app/scripts/version.js
@@ -1,5 +1,5 @@
 var releasedVersion = '2.5.2';
-var downloadLink    = 'https://drive.google.com/file/d/1JaJoXQiVqds3Wt3szPCRnrDS-T1-H9QT/view';
+var downloadLink    = 'href';
 
 function updatePublishedVersion(releasedVersion) {
   var publishedVersion = '2.5.2';
@@ -17,7 +17,7 @@ function updateDocumentation() {
   $("#aboutNewRelease").html("\
     <br/>\
     <li>Updated development tools images and the logo. Changed word \"Developing\" to \"Development\"</li><br/>\
-    <li>Updated file updates.html. version.js used to use rawgit.com. Now domain has been changed to cdn.jsdelivr.net<br/>\
+    <li>Updated file updates.html. version.js used to use rawgit.com. Now domain has been changed to raw.githack.com<br/>\
     and also old repository links have been replaced to \"https://github.com/gubrus50/Minecraft-Development-Tools/...\"</li><br />\
     <li>New release notes have been updated. \"Now Download released version\" link is always updated from github.com/.../MDT/scripts/version.js</li><br/>\
   ");
