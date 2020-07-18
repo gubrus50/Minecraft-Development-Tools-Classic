97eed7194e9bcb095f97c06e650f779cc565dcd2
McRaZick <mcrazick@mail.com>
2020-07-18 20:19:02 +0100

    Initialized Electron with ReactJS

---
+++ Minecraft-DT2/mdt/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p><span class="title">Minecraft-DT2</span><br/>.NEXT GENERATION</p>
+    </div>
+  );
+}
+
+export default App;
